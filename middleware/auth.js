c8473328ac491972a3e21b8968c0a06df53d7402
Michael Oki <oki.mikael@outlook.com>
2020-04-30 20:13:58 +0700

    registrasi berhasil

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,27 +3,40 @@ var mysql = require('mysql');
 var md5 = require('MD5');
 var response =require('../res');
 var jwt = require('jsonwebtoken');
+var config = require('../controller')
 var config = require('../config/secret');
 var ip = require('ip');
 
 //controller untuk registrasi
 exports.registrasi = function(req, res) {
     var post = {
-        username: req.body.username,
+        nama_user: req.body.username,
         email: req.body.email,
         password: md5(req.body.password),
-        exports.registrasi = function(req, res) {
+        level: req.body.level
+    }
+
+    var query = "SELECT email FROM ?? WHERE ??";
+    var table = ["t_user", "email", post.email];
+
+    query = mysql.format(query,table);
+
+    connection.query(query, function(error,rows){
+        if(error){
             console.log(error);
         }else{
             if(rows.length == 0){
                 var query = "INSERT INTO ?? SET ?";
-                var table = ["user"];
+                var table = ["t_user"];
                 query = mysql.format(query,table);
-                connection.query(query, post, function(error, rows){
+                connection.query(query, post, function(error,rows){
                     if(error){
                         console.log(error);
                     }else{
-            exports.registrasi = function(req, res) {
+                        response.ok("Berhasil menambahkan data user baru", res);
+                    }
+                });
+            }else{
                 response.ok("Email sudah terdaftar!");
             }
         }
